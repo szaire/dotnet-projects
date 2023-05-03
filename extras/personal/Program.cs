b6688fa507e7c30fc8eb9d8ab15f5951fcb247ac
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-03 08:49:42 -0300

    add new course files

---
+++ extras/personal/Program.cs
@@ -0,0 +1,33 @@
+﻿int squareSize = 10;
+for (int i = 0; i < squareSize; i++)
+{
+    for (int j = 0; j < squareSize; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < squareSize; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < squareSize; i++)
+{
+    for (int j = squareSize-1; j > i; j--)
+    {
+        Console.Write("  ");
+    }
+    for (int k = 0; k <= i; k++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
