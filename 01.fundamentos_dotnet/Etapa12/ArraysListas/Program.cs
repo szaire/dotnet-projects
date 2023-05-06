09ca99c4d22fc4e3152067b0b9d0546f4146000f
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-05 21:36:35 -0300

    enhanced Program.cs code structure

--- 01.fundamentos_dotnet/Etapa12/ArraysListas/Program.cs
+++ 01.fundamentos_dotnet/Etapa12/ArraysListas/Program.cs
@@ -1,18 +1,49 @@
-﻿int[] arr = new int[] {23, 47, 12, 9};
-
-// Loop em array com For
-for (int i = 0; i < arr.Length; i++)
+﻿internal class Program
 {
-    Console.WriteLine($"Posição nº {i} - {arr[i]}");
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[] { 23, 47, 12, 9 };
+
+        // Loop em array com For
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Posição nº {i} - {arr[i]}");
+        }
+
+        Console.WriteLine();
+
+        // Loop em array com Foreach
+        foreach (int var in arr)
+        {
+            Console.WriteLine(var);
+        }
+
+        Console.WriteLine();
+        
+        // Método para imprimir array em tela;
+        printArray(ref arr);
+
+        // Redimensionando um Array
+        Array.Resize(ref arr, arr.Length * 2);
+        Console.Write("Array Original: ");
+        printArray(ref arr);
+
+        // Redimensionando um Array com copy
+        int[] newArr = new int[arr.Length];
+        Array.Copy(arr, newArr, arr.Length);
+        Console.Write("Array Copia: ");
+        printArray(ref newArr);
+
+
+    }
+
+    public static void printArray(ref int[] arr)
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr.Length - i == 1) Console.WriteLine($"{arr[i]} ]");
+            else Console.Write($"{arr[i]}, ");
+        }
+    }
 }
-
-Console.WriteLine();
-
-// Loop em array com Foreach
-foreach (int var in arr)
-{
-    Console.WriteLine(var);
-}
-
-// Redimensionando um Array
-Array.Resize(ref arr, arr.Length * 2);
