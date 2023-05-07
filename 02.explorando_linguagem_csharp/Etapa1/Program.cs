62b4d9f1212bd94ba69b5d1a716d412db1ee9218
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-07 16:52:35 -0300

    add courses second mdoule

---
+++ 02.explorando_linguagem_csharp/Etapa1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Etapa1.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+
+        // Aqui se está utilizando o método "set" das propriedades
+        // "Nome" e "Idade" ao evidenciar o operador de atribuição
+        p1.Nome = "Samuel Zaire";
+        p1.Idade = 21;
+
+        p1.Apresentar();
+    }
+}
